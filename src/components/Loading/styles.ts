654a419d3679423bb9a3ae6f6b28ea55c898e474
hulysses <hulyssesmaga@gmail.com>
2025-03-10 18:06:19 -0300

    add LoadIndicator styled component to Loading styles

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -1,7 +1,13 @@
-import styled from "styled-components/native";
+import styled, { DefaultTheme } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
+
+export const LoadIndicator = styled.ActivityIndicator.attrs(
+  ({ theme }: { theme: DefaultTheme }) => ({
+    color: theme.COLORS.BLUE_DARK_400,
+  })
+);
