d9c73f922740e273fff3cf39ccc2a8915719c658
hulysses <hulyssesmaga@gmail.com>
2025-03-11 19:12:11 -0300

    correct navigation.d.ts export

--- src/app/@types/navigation.d.ts
+++ src/app/@types/navigation.d.ts
@@ -1,8 +1,9 @@
-export declare global {
-    namespace ReactNavigation {
-        interface RootParamList {
-        home: undefined;
-        register: undefined;
-        }
+export {};
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined;
+      register: undefined;
     }
+  }
 }
